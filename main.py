488b73801267f56e3c371ca4ee61bf0f0e500658
Laura Moreno <laura.moreno.sabate@gmail.com>
2023-04-25 01:15:06 +0200

    uploading

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import src.extraction as extract
 import src.cleaning as clean
 import src.visualization as viz
@@ -8,7 +9,7 @@ df_clean = clean.basic_cleaning_1(df)
 df_clean_question_1 = clean.cleaning_question_1(df_clean)
 df_clean_question_2 = clean.cleaning_question_2(df_clean)
 df_clean_question_3 = clean.cleaning_question_3(df_clean)
-viz.visualizing_question_1(df_clean_question_1, "graph_1")
-viz.visualizing_question_2_a(df_clean_question_2, "graph_2")
-viz.visualizing_question_2_b(df_clean_question_2, "graph_3")
-viz.visualizing_question_3(df_clean_question_2, "graph_4")
+viz.visualizing_question_1(df_clean_question_1)
+viz.visualizing_question_2_a(df_clean_question_2)
+viz.visualizing_question_2_b(df_clean_question_2)
+viz.visualizing_question_3(df_clean_question_2)
